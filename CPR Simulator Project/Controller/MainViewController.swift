4830f2d581dbb642f08258e7d890cc43232e7fe2
chimdolin <chimdolin.me@gmail.com>
2017-10-16 10:19:15 +0700

    init commit

---
+++ CPR Simulator Project/Controller/MainViewController.swift
@@ -0,0 +1,24 @@
+//
+//  MainViewController.swift
+//  CPR Simulator Project
+//
+//  Created by chimdolin on 14/10/2560 BE.
+//  Copyright © 2560 chimdolin. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
---
+++ CPR Simulator ProjectTests/CPR_Simulator_ProjectTests.swift
@@ -0,0 +1,36 @@
+//
+//  CPR_Simulator_ProjectTests.swift
+//  CPR Simulator ProjectTests
+//
+//  Created by chimdolin on 11/10/2560 BE.
+//  Copyright © 2560 chimdolin. All rights reserved.
+//
+
+import XCTest
+@testable import CPR_Simulator_Project
+
+class CPR_Simulator_ProjectTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
