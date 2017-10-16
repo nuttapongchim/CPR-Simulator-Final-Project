b93fd3f48dd250c0377cc3d818b697d7eead9760
chimdolin <chimdolin.me@gmail.com>
2017-10-16 14:51:45 +0700

    modified auto layout

--- CPR Simulator Project/Controller/MainViewController.swift
+++ CPR Simulator Project/Controller/MainViewController.swift
@@ -10,10 +10,39 @@ import UIKit
 
 class MainViewController: UIViewController {
 
+    @IBOutlet weak var whatCPRBtn: UIButton!
+    @IBOutlet weak var howToCPRBtn: UIButton!
+    @IBOutlet weak var startCPRBtn: UIButton!
+    @IBOutlet weak var headerTransitionView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        //custom button
+        whatCPRBtn.layer.cornerRadius = 3
+        whatCPRBtn.layer.borderWidth = 1
+        whatCPRBtn.layer.borderColor = UIColor(red:0.70, green:0.62, blue:0.85, alpha:1.00).cgColor
+        whatCPRBtn.layer.shadowColor = UIColor(red:0.64, green:0.82, blue:0.88, alpha:1.00).cgColor
+        whatCPRBtn.layer.shadowRadius = 3
+        whatCPRBtn.layer.shadowOpacity = 1
+        whatCPRBtn.layer.shadowOffset = CGSize(width:3 ,height:3)
+        
+        howToCPRBtn.layer.cornerRadius = 3
+        howToCPRBtn.layer.borderWidth = 1
+        howToCPRBtn.layer.borderColor = UIColor(red:0.70, green:0.62, blue:0.85, alpha:1.00).cgColor
+        howToCPRBtn.layer.shadowColor = UIColor(red:0.64, green:0.82, blue:0.88, alpha:1.00).cgColor
+        howToCPRBtn.layer.shadowRadius = 3
+        howToCPRBtn.layer.shadowOpacity = 1
+        howToCPRBtn.layer.shadowOffset = CGSize(width:3 ,height:3)
+        
+        startCPRBtn.layer.cornerRadius = 3
+        startCPRBtn.layer.borderWidth = 1
+        startCPRBtn.layer.borderColor = UIColor(red:0.70, green:0.62, blue:0.85, alpha:1.00).cgColor
+        startCPRBtn.layer.shadowColor = UIColor(red:0.64, green:0.82, blue:0.88, alpha:1.00).cgColor
+        startCPRBtn.layer.shadowRadius = 3
+        startCPRBtn.layer.shadowOpacity = 1
+        startCPRBtn.layer.shadowOffset = CGSize(width:3 ,height:3)
+        
     }
 
     override func didReceiveMemoryWarning() {
